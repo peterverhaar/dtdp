d0f535c09fafe29e93b1151dd2bd5161d4fda229
peterverhaar <peter.verhaar@gmail.com>
2019-02-12 11:06:37 +0100

    Update cc1.py

--- CodingChallenges/cc1.py
+++ CodingChallenges/cc1.py
@@ -1,1 +1,17 @@
 
+
+number = 17
+
+
+guess = int( input("Guess a number in between 1 and 50: ") )
+
+while guess != number:
+
+    if guess > number:
+        print("Lower!")
+    else:
+        print("Higher!")
+    guess = int( input("Guess again!: ") )
+
+print("The correct number is indeed {}.".format(number) )
+
